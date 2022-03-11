52b7ef384c2c3e925f39f5a08e27816dc9adef2d
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2022-03-11 12:52:44 +0300

    adding html files

--- auctionSite/views.py
+++ auctionSite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "base.html")
 
 def about(request):
     return render(request, "about.html")
--- main/settings.py
+++ main/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CRISPY_TEMPLATE_PACK='bootstrap4'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
